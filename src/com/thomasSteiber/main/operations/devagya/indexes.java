e06f76fa214442dfe8164ce9b59c6c91d9eb0d79
madHEYsia <submiitr07@gmail.com>
2018-04-28 14:40:19 +0530

    junk - WIP

---
+++ src/com/thomasSteiber/main/operations/devagya/indexes.java
@@ -0,0 +1,12 @@
+package com.thomasSteiber.main.operations.devagya;
+
+public class indexes {
+
+    public static int totalIndexes = 6;
+    public static int depthIndex = 0;
+    public static int grIndex = 1;
+    public static int vshaleIndex = 2;
+    public static int nPhiIndex = 3;
+    public static int rhobIndex = 4;
+    public static int resIndex = 5;
+}
